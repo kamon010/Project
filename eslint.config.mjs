c32e6814364df831f4ea0d91fd9b719dd657f63e
Kamon <kamonratmeesak@MacBookAirKamon.local>
2024-09-17 14:48:33 +0700

    up lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,11 @@ const require = createRequire(
     import.meta.url);
 
 import { ESLint } from 'eslint';
+import parser from '@typescript-eslint/parser'; // แก้ไขส่วนนี้
 
 export default {
     languageOptions: {
-        parser: require.resolve('@typescript-eslint/parser'),
+        parser: parser, // ใช้ parser ที่ import มา
     },
     plugins: {
         '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
