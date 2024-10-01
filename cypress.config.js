bef52c928e91b1824cde281e6cefc030d366023c
Kamon <kamonratmeesak@MacBookAirKamon.local>
2024-10-01 17:36:14 +0700

    up e2e

--- cypress.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  e2e: {
-    specPattern: "cypress/integration/e2e_test.js",
-    supportFile: false, // If you're not using a support file, it's fine to leave it as false
-  },
-};
