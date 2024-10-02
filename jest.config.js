f295ce23801b5bb64113f7792bebf86a7df9b404
Kamon <kamonratmeesak@MacBookAirKamon.local>
2024-10-02 14:48:21 +0700

    run unit

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
+  verbose: true,
   testEnvironment: "jsdom", // ใช้ jsdom สำหรับการทดสอบ DOM
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  testMatch: ["**/tests/**/*.test.js"], // รันเฉพาะไฟล์ที่อยู่ในโฟลเดอร์ tests
 };
